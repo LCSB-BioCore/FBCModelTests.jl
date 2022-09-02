0e7744e5c651cfdd47471bc52c08f9a2e8a1229e
St. Elmo Wilken <stelmozors@gmail.com>
2022-09-02 19:58:19 +0200

    add tests and format

--- src/memote.jl
+++ src/memote.jl
@@ -9,22 +9,33 @@ https://doi.org/10.1038/s41587-020-0446-y for details.
 $(TYPEDSIGNATURES)
 
 Iterates through all the reactions in `model` and checks if the charges across
-each reaction balance. Returns a list of reaction IDs that are charge unbalanced, 
-which is empty if the test passes. 
+each reaction balance. Returns a list of reaction IDs that are charge
+unbalanced, which is empty if the test passes. 
+
+Optionally, pass a list of reactions to ignore in this process through
+`ignored_reactions`. It makes sense to include the biomass and 
+exchange reactions in this list (default).
 """
-function is_model_charge_balanced(model::COBREXA.MetabolicModel)
+function is_model_charge_balanced(
+    model::COBREXA.MetabolicModel;
+    ignored_reactions = [
+        find_biomass_reaction_ids(model)
+        find_exchange_reaction_ids(model)
+    ],
+)
     unbalanced_rxns = String[]
 
     for rid in reactions(model)
+        rid in ignored_reactions && continue
         _rbal = 0
-        for (mid, stoich) in reacton_stoichiometry(model, rid)
+        for (mid, stoich) in reaction_stoichiometry(model, rid)
             try
                 _rbal += metabolite_charge(model, mid) * stoich
-            catch 
+            catch
                 throw(error("Something is wrong with reaction $rid."))
             end
         end
-        !approx(_rbal, 0) && push!(unbalanced_rxns, rid) 
+        !isapprox(_rbal, 0) && push!(unbalanced_rxns, rid)
     end
 
     return unbalanced_rxns
@@ -36,12 +47,23 @@ $(TYPEDSIGNATURES)
 Iterates through all the reactions in `model` and checks if the mass across each
 reaction balances. Returns a list of reaction IDs that are mass unbalanced, which
 is empty if the test passes.
+
+Optionally, pass a list of reactions to ignore in this process through
+`ignored_reactions`. It makes sense to include the biomass and 
+exchange reactions in this list (default).
 """
-function is_model_mass_balanced(model::COBREXA.MetabolicMode)
+function is_model_mass_balanced(
+    model::COBREXA.MetabolicModel;
+    ignored_reactions = [
+        find_biomass_reaction_ids(model)
+        find_exchange_reaction_ids(model)
+    ],
+)
     unbalanced_rxns = String[]
-    
+
     for rid in reactions(model)
-        try 
+        rid in ignored_reactions && continue
+        try
             _rbal = reaction_atom_balance(model, rid)
             all(values(_rbal) .== 0) || push!(unbalanced_rxns, rid)
         catch
---
+++ test/runtests.jl
@@ -0,0 +1,11 @@
+using Test
+using FBCModelTests
+using COBREXA
+
+isfile("e_coli_core.json") ||
+    download("http://bigg.ucsd.edu/static/models/e_coli_core.json", "e_coli_core.json")
+model = load_model("e_coli_core.json")
+
+@testset "Test tests" begin
+    include("memote.jl")
+end
