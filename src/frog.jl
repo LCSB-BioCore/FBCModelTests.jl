c5c756252e1efadbb176579676966257980a1236
Mirek Kratochvil <exa.exa@gmail.com>
2022-10-12 20:33:45 +0200

    support multiple objectives in FROG, clean up the code

--- src/frog.jl
+++ src/frog.jl
@@ -1,71 +1,63 @@
-"""
-$(TYPEDSIGNATURES)
+
+#TODO any other kinds of suboptimal solutions?
+objstatus(::Nothing) = "infeasible"
+objstatus(::Any) = "optimal"
+
+objvalue(::Nothing) = ""
+objvalue(x::Number) = string(x)
 
 """
-function generate_frog_report(filename::String; kwargs...)
-    generate_frog_report(load_sbml_model(filename), filename; kwargs...)
+$(TYPEDEF)
+
+# Fields
+$(TYPEDFIELDS)
+"""
+struct ResetObjective <: ModelWrapper
+    model::MetabolicModel
+    objective::SparseVec
 end
 
+COBREXA.unwrap_model(x::ResetObjective) = x.model
+COBREXA.objective(x::ResetObjective) = x.objective
+
 """
 $(TYPEDSIGNATURES)
 
+Generate a [`FROGObjectiveReport`](@ref) containing the reproducibility data
+for a single objective in the SBML model.
 """
-function generate_frog_report(
-    model::SBMLModel,
-    filename::String;
-    report_dir::String,
+function frog_objective_report(
+    sbml_model::SBMLModel,
+    objective::String;
     optimizer,
-    workers = [Distributed.myid()],
-    basefilename::String = basename(filename),
-)
-    @info "Generating FROG report for $filename..."
-    mkpath(report_dir)
+    workers,
+)::FROGObjectiveReport
 
-    outname(x) = joinpath(report_dir, x)
+    @info "Creating report for objective $objective ..."
+    # this prevents the default SBMLModel fireworks in case there's multiple objectives
+    model = ResetObjective(sbml_model, SBML.fbc_flux_objective(sbml_model.sbml, objective))
 
-    @info "Finding model objective value..."
-    n = n_reactions(model)
+    # run the first FBA
+    @info "Finding model objective value ..."
     solved_model = flux_balance_analysis(model, optimizer)
     obj = solved_objective_value(solved_model)
-    open(outname("01_objective.tsv"), "w") do f
-        writedlm(
-            f,
-            [
-                "model" "objective" "status" "value"
-                basefilename "obj" objstatus(obj) objvalue(obj)
-            ],
+
+    fvas = if isnothing(obj)
+        @warn "Model does not have a feasible solution, skipping FVA."
+        zeros(0, 2)
+    else
+        @info "Optimal solution found." obj
+        @info "Calculating model variability ..."
+        flux_variability_analysis(
+            model,
+            optimizer;
+            bounds = objective_bounds(1.0),
+            optimal_objective_value = obj,
+            workers = workers,
         )
     end
 
-    isnothing(obj) && throw("Model does not have an objective value, can't continue.")
-
-    @info "Calculating model variability..."
-    fvas = flux_variability_analysis(
-        model,
-        optimizer;
-        bounds = objective_bounds(1.0),
-        optimal_objective_value = obj,
-        workers = workers,
-    )
-    open(outname("02_fva.tsv"), "w") do f
-        writedlm(
-            f,
-            [
-                "model" "objective" "reaction" "flux" "status" "minimum" "maximum"
-                hcat(
-                    fill(basefilename, n),
-                    fill("obj", n),
-                    reactions(model),
-                    objvalue.(flux_vector(model, solved_model)),
-                    objstatus.(fvas[:, 1]),
-                    objvalue.(fvas[:, 1]),
-                    objvalue.(fvas[:, 2]),
-                )
-            ],
-        )
-    end
-
-    @info "Calculating gene knockouts..."
+    @info "Calculating gene knockouts ..."
     gs = screen(
         model,
         args = tuple.(genes(model)),
@@ -74,25 +66,8 @@ function generate_frog_report(
         ),
         workers = workers,
     )
-    ng = n_genes(model)
 
-    open(outname("03_gene_deletion.tsv"), "w") do f
-        writedlm(
-            f,
-            [
-                "model" "objective" "gene" "status" "value"
-                hcat(
-                    fill(basefilename, ng),
-                    fill("obj", ng),
-                    genes(model),
-                    objstatus.(gs),
-                    objvalue.(gs),
-                )
-            ],
-        )
-    end
-
-    @info "Calculating reaction knockouts..."
+    @info "Calculating reaction knockouts ..."
     rs = screen(
         model,
         args = tuple.(reactions(model)),
@@ -106,48 +81,158 @@ function generate_frog_report(
         workers = workers,
     )
 
-    open(outname("04_reaction_deletion.tsv"), "w") do f
+    @info "Objective $objective done."
+    return FROGObjectiveReport(
+        optimum = obj,
+        flux = flux_vector(model, solved_model),
+        variabilities_min = fvas[:, 1],
+        variabilities_max = fvas[:, 2],
+        reaction_knockouts = rs,
+        gene_knockouts = gs,
+    )
+end
+
+"""
+$(TYPEDSIGNATURES)
+
+Generate [`FROGReportData`](@ref) for a model.
+"""
+frog_model_report(model::SBMLModel; optimizer, workers = [Distributed.myid()]) =
+    FROGReportData(
+        reactions = reactions(model),
+        genes = genes(model),
+        objectives = Dict(
+            objective => frog_objective_report(model, objective; optimizer, workers) for
+            (objective, _) in model.sbml.objectives
+        ),
+    )
+
+"""
+$(TYPEDSIGNATURES)
+
+Write the contents of [`FROGReportData`](@ref) to the 4 TSV files as specified
+by FROG standard, and additionally write the metadata into the JSON file.
+"""
+function frog_write_to_directory(
+    r::FROGReportData,
+    metadata::Dict;
+    report_dir::String,
+    basefilename::String,
+)
+    outname(x) = joinpath(report_dir, x)
+    writeto(x::Function, fn) = open(x, outname(fn), "w")
+    squash(x) = permutedims(hcat(x...), (2, 1))
+
+    mkpath(report_dir)
+
+    writeto("01_objective.tsv") do f
         writedlm(
             f,
             [
-                "model" "objective" "reaction" "status" "value"
-                hcat(
-                    fill(basefilename, n),
-                    fill("obj", n),
-                    reactions(model),
-                    objstatus.(rs),
-                    objvalue.(rs),
+                "model" "objective" "status" "value"
+                squash(
+                    [basefilename, obj, objstatus(o.optimum), objvalue(o.optimum)] for
+                    (obj, o) in r.objectives
                 )
             ],
         )
     end
 
-    @info "Writing the JSON metadata..."
-    open(outname("00_metadata.json"), "w") do f
-        JSON.print(
+    writeto("02_fva.tsv") do f
+        writedlm(
             f,
-            Dict(
-                "software.name" => "FBCModelTests.jl",
-                "software.version" => string(FBCMT_VERSION),
-                "software.url" => "https://github.com/LCSB-BioCore/FBCModelTests.jl/",
-                "environment" => begin
-                    x = IOBuffer()
-                    InteractiveUtils.versioninfo(x)
-                    replace(String(take!(x)), r"\n *" => " ")
-                end,
-                "model.filename" => basefilename,
-                "model.md5" => bytes2hex(open(f -> md5(f), filename, "r")),
-                "solver.name" => "COBREXA.jl $COBREXA_VERSION ($(COBREXA.JuMP.MOI.get(optimizer(), COBREXA.JuMP.MOI.SolverName())))",
-            ),
-            2,
+            [
+                "model" "objective" "reaction" "flux" "status" "minimum" "maximum"
+                squash(
+                    [
+                        basefilename,
+                        obj,
+                        rxn,
+                        objvalue(flx),
+                        objstatus(varmin),
+                        objvalue(varmin),
+                        objvalue(varmax),
+                    ] for (obj, o) = r.objectives if !isnothing(o.optimum) for
+                    (rxn, flx, varmin, varmax) in
+                    zip(r.reactions, o.flux, o.variabilities_min, o.variabilities_max)
+                )
+            ],
         )
     end
-    @info "Generated FROG report for $filename into $report_dir"
+
+    writeto("03_gene_deletion.tsv") do f
+        writedlm(
+            f,
+            [
+                "model" "objective" "gene" "status" "value"
+                squash(
+                    [basefilename, obj, gene, objstatus(geneval), objvalue(geneval)] for
+                    (obj, o) in r.objectives for
+                    (gene, geneval) in zip(r.genes, o.gene_knockouts)
+                )
+            ],
+        )
+    end
+
+    writeto("04_reaction_deletion.tsv") do f
+        writedlm(
+            f,
+            [
+                "model" "objective" "reaction" "status" "value"
+                squash(
+                    [basefilename, obj, rxn, objstatus(rxnval), objvalue(rxnval)] for
+                    (obj, o) in r.objectives for
+                    (rxn, rxnval) in zip(r.reactions, o.reaction_knockouts)
+                )
+            ],
+        )
+    end
+
+    writeto("00_metadata.json") do f
+        JSON.print(f, metadata, 2)
+    end
+
+    nothing
 end
 
-#TODO properly mark various other kinds of suboptimal solutions
-objstatus(::Nothing) = "infeasible"
-objstatus(::Any) = "optimal"
+"""
+$(TYPEDSIGNATURES)
+"""
+frog_metadata(filename::String; optimizer, basefilename::String = basename(filename)) =
+    Dict(
+        "software.name" => "FBCModelTests.jl",
+        "software.version" => string(FBCMT_VERSION),
+        "software.url" => "https://github.com/LCSB-BioCore/FBCModelTests.jl/",
+        "environment" => begin
+            x = IOBuffer()
+            InteractiveUtils.versioninfo(x)
+            replace(String(take!(x)), r"\n *" => " ")
+        end,
+        "model.filename" => basefilename,
+        "model.md5" => bytes2hex(open(f -> md5(f), filename, "r")),
+        "model.sha256" => bytes2hex(open(f -> sha256(f), filename, "r")),
+        "solver.name" => "COBREXA.jl $COBREXA_VERSION ($(COBREXA.JuMP.MOI.get(optimizer(), COBREXA.JuMP.MOI.SolverName())))",
+    )
 
-objvalue(::Nothing) = ""
-objvalue(x::Number) = string(x)
+"""
+$(TYPEDSIGNATURES)
+"""
+function frog_generate_report(
+    filename::String;
+    report_dir::String,
+    optimizer,
+    workers = [Distributed.myid()],
+    basefilename::String = basename(filename),
+)
+    m = load_sbml_model(filename)
+    @info "Generating FROG report for $filename ..."
+    r = frog_model_report(m; optimizer, workers)
+
+    @info "Generating metadata for $filename ..."
+    metadata = frog_metadata(filename; optimizer, basefilename)
+
+    @info "Writing output to $report_dir ..."
+    frog_write_to_directory(r, metadata; report_dir, basefilename)
+
+    @info "FROG report done for $filename."
+end
