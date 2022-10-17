a4da05eee3d6cd0c097669dbc338d190905403f9
St. Elmo Wilken <stelmozors@gmail.com>
2022-10-17 23:01:59 +0200

    remove ack and only include in one place

--- src/memote/reactions.jl
+++ src/memote/reactions.jl
@@ -1,10 +1,3 @@
-#=
-This file contains a collection of tests based on Memote. See Lieven, C., Beber,
-M.E., Olivier, B.G. et al. MEMOTE for standardized genome-scale metabolic model
-testing. Nat Biotechnol 38, 272–276 (2020).
-https://doi.org/10.1038/s41587-020-0446-y for details.
-=#
-
 """
 $(TYPEDSIGNATURES)
 
