68bd15a2b30da920453d06977a736065a81f2985
Mirek Kratochvil <exa.exa@gmail.com>
2022-10-18 11:15:30 +0200

    split FROG code into files and a directory

---
+++ src/frog/frontend.jl
@@ -0,0 +1,47 @@
+
+"""
+$(TYPEDSIGNATURES)
+
+A complete function for one-shot generation of FROG reports. Use
+[`frog_model_report`](@ref), [`frog_metadata`](@ref) and
+[`frog_write_to_directory`](@ref) for finer control of the process.
+"""
+function frog_generate_report(
+    filename::String;
+    report_dir::String,
+    optimizer,
+    workers = [Distributed.myid()],
+    basefilename::String = basename(filename),
+)
+    m = load_sbml_model(filename)
+    @info "Generating FROG report for $filename ..."
+    r = frog_model_report(m; optimizer, workers)
+
+    @info "Generating metadata for $filename ..."
+    metadata = frog_metadata(filename; optimizer, basefilename)
+
+    @info "Writing output to $report_dir ..."
+    frog_write_to_directory(r, metadata; report_dir, basefilename)
+
+    @info "FROG report done for $filename."
+end
+
+"""
+$(TYPEDSIGNATURES)
+
+A simple wrapper for comparing 2 previously generated reports in their
+respective directories.
+"""
+function frog_compare_reports(report_dir_a::String, report_dir_b::String)
+    a = frog_read_from_directory(report_dir_a)
+    b = frog_read_from_directory(report_dir_b)
+
+    @testset "Comparing FROG reports in $report_dir_a and $report_dir_b" begin
+        @testset "Metadata" begin
+            frog_test_metadata_compatibility(a.metadata, b.metadata)
+        end
+        @testset "Objectives and solution values" begin
+            frog_test_report_equality(a.report, b.report)
+        end
+    end
+end
---
+++ src/frog/report.jl
@@ -0,0 +1,193 @@
+
+"""
+$(TYPEDEF)
+
+# Fields
+$(TYPEDFIELDS)
+"""
+struct ResetObjective <: ModelWrapper
+    model::MetabolicModel
+    objective::SparseVec
+end
+
+COBREXA.unwrap_model(x::ResetObjective) = x.model
+COBREXA.objective(x::ResetObjective) = x.objective
+
+"""
+$(TYPEDSIGNATURES)
+
+Generate a [`FROGObjectiveReport`](@ref) containing the reproducibility data
+for a single objective in the SBML model.
+"""
+function frog_objective_report(
+    sbml_model::SBMLModel,
+    objective::String;
+    optimizer,
+    workers,
+)::FROGObjectiveReport
+    @info "Creating report for objective $objective ..."
+    # this prevents the default SBMLModel fireworks in case there's multiple objectives
+    model = ResetObjective(sbml_model, SBML.fbc_flux_objective(sbml_model.sbml, objective))
+
+    # run the first FBA
+    @info "Finding model objective value ..."
+    solved_model = flux_balance_analysis(model, optimizer)
+    obj = solved_objective_value(solved_model)
+
+    fvas = if isnothing(obj)
+        @warn "Model does not have a feasible solution, skipping FVA."
+        zeros(0, 2)
+    else
+        @info "Optimal solution found." obj
+        @info "Calculating model variability ..."
+        flux_variability_analysis(
+            model,
+            optimizer;
+            bounds = objective_bounds(1.0),
+            optimal_objective_value = obj,
+            workers = workers,
+        )
+    end
+
+    @info "Calculating gene knockouts ..."
+    gids = genes(model)
+    gs = screen(
+        model,
+        args = tuple.(gids),
+        analysis = (m, gene) -> solved_objective_value(
+            flux_balance_analysis(m, optimizer, modifications = [knockout(gene)]),
+        ),
+        workers = workers,
+    )
+
+    @info "Calculating reaction knockouts ..."
+    rids = reactions(model)
+    rs = screen(
+        model,
+        args = tuple.(rids),
+        analysis = (m, rid) -> solved_objective_value(
+            flux_balance_analysis(
+                m,
+                optimizer,
+                modifications = [change_constraint(rid, lb = 0.0, ub = 0.0)],
+            ),
+        ),
+        workers = workers,
+    )
+
+    @info "Objective $objective done."
+    return FROGObjectiveReport(
+        optimum = obj,
+        reactions = Dict(
+            rid => FROGReactionReport(
+                flux = flx,
+                variability_min = vmin,
+                variability_max = vmax,
+                deletion = ko,
+            ) for (rid, flx, vmin, vmax, ko) in
+            zip(rids, flux_vector(model, solved_model), fvas[:, 1], fvas[:, 2], rs)
+        ),
+        gene_deletions = Dict(gids .=> gs),
+    )
+end
+
+"""
+$(TYPEDSIGNATURES)
+
+Generate [`FROGReportData`](@ref) for a model.
+"""
+frog_model_report(model::SBMLModel; optimizer, workers = [Distributed.myid()]) = Dict([
+    objective => frog_objective_report(model, objective; optimizer, workers) for
+    (objective, _) in model.sbml.objectives
+])
+
+"""
+$(TYPEDSIGNATURES)
+"""
+function frog_test_report_equality(
+    a::FROGReportData,
+    b::FROGReportData;
+    absolute_tolerance = 1e-6,
+    relative_tolerance = 1e-4,
+)
+    intol(a, b) =
+        (isnothing(a) && isnothing(b)) || (
+            !isnothing(a) &&
+            !isnothing(b) &&
+            abs(a - b) <= absolute_tolerance &&
+            (
+                (a * b > 0) && abs(a * (1 + relative_tolerance)) >= abs(b) ||
+                abs(b * (1 + relative_tolerance)) >= abs(a)
+            )
+        )
+
+    @testset "Comparing objectives" begin
+        test_dicts(
+            (_, a, b) -> begin
+                @test intol(a.optimum, b.optimum)
+                @testset "Reactions" begin
+                    test_dicts(
+                        (_, a, b) -> begin
+                            @test intol(a.flux, b.flux)
+                            @test intol(a.variability_min, b.variability_min)
+                            @test intol(a.variability_max, b.variability_max)
+                            @test intol(a.deletion, b.deletion)
+                        end,
+                        a.reactions,
+                        b.reactions,
+                    )
+                end
+                @testset "Gene deletions" begin
+                    test_dicts(
+                        (_, a, b) -> begin
+                            @test intol(a, b)
+                        end,
+                        a.gene_deletions,
+                        b.gene_deletions,
+                    )
+                end
+            end,
+            a,
+            b,
+        )
+    end
+end
+
+"""
+$(TYPEDSIGNATURES)
+"""
+frog_metadata(filename::String; optimizer, basefilename::String = basename(filename)) =
+    FROGMetadata(
+        "software.name" => "FBCModelTests.jl",
+        "software.version" => string(FBCMT_VERSION),
+        "software.url" => "https://github.com/LCSB-BioCore/FBCModelTests.jl/",
+        "environment" => begin
+            x = IOBuffer()
+            InteractiveUtils.versioninfo(x)
+            replace(String(take!(x)), r"\n *" => " ")
+        end,
+        "model.filename" => basefilename,
+        "model.md5" => bytes2hex(open(f -> md5(f), filename, "r")),
+        "model.sha256" => bytes2hex(open(f -> sha256(f), filename, "r")),
+        "solver.name" => "COBREXA.jl $COBREXA_VERSION ($(COBREXA.JuMP.MOI.get(optimizer(), COBREXA.JuMP.MOI.SolverName())))",
+    )
+
+"""
+$(TYPEDSIGNATURES)
+"""
+function frog_test_metadata_compatibility(a::FROGMetadata, b::FROGMetadata)
+    for k in ["model.filename", "model.md5"]
+        @testset "$k is present" begin
+            @test haskey(a, k)
+            @test haskey(b, k)
+        end
+    end
+
+    for k in ["model.filename", "model.md5", "model.sha256"]
+        if haskey(a, k) && haskey(b, k)
+            @testset "$k matches" begin
+                @test a[k] == b[k]
+            end
+        end
+    end
+end
