a731c1d4828513c29944d04edcb55bfa637aa3db
Mirek Kratochvil <exa.exa@gmail.com>
2022-10-18 11:04:46 +0200

    test: download and verify the files systematically
    Closes #42

--- test/data.jl
+++ test/data.jl
@@ -1,13 +1,47 @@
 
-# tried and trusted E. coli core
-isfile("e_coli_core.json") || Downloads.download(
-    "http://bigg.ucsd.edu/static/models/e_coli_core.json",
-    "e_coli_core.json",
+datadir = joinpath(@__DIR__, "testdata")
+mkpath(datadir)
+
+model_file = Dict(
+    (
+        begin
+            fn = joinpath(datadir, filename)
+            if !isfile(fn)
+                Downloads.download(url, fn)
+            end
+
+            cksum = open(fn) do io
+                bytes2hex(sha256(io))
+            end
+
+            if cksum != hash
+                @error "Hash mismatch for `$filename'" downloaded = cksum expected = hash
+                @info "Remove test/$fn to force re-downloading"
+                error("Downloaded hash mismatch for `$filename'")
+            end
+
+            filename => fn
+
+        end
+    ) for (filename, url, hash) in [
+        (
+            "e_coli_core.json",
+            "http://bigg.ucsd.edu/static/models/e_coli_core.json",
+            "7bedec10576cfe935b19218dc881f3fb14f890a1871448fc19a9b4ee15b448d8",
+        ),
+        (
+            "e_coli_core.xml",
+            "http://bigg.ucsd.edu/static/models/e_coli_core.xml",
+            "b4db506aeed0e434c1f5f1fdd35feda0dfe5d82badcfda0e9d1342335ab31116",
+        ),
+        (
+            "iJN746.json",
+            "http://bigg.ucsd.edu/static/models/iJN746.json",
+            "3da875419f4edff3e3e27424742b8b410b6b778eb0955b8939ae933fad5f1219",
+        ),
+    ]
 )
-model = load_model("e_coli_core.json")
 
-# this model has an energy generating cycle
-isfile("iJN746.json") ||
-    Downloads.download("http://bigg.ucsd.edu/static/models/iJN746.json", "iJN746.json")
-iJN746 = load_model("iJN746.json")
-
+#TODO remove the globals
+model = load_model(model_file["e_coli_core.json"])
+iJN746 = load_model(model_file["iJN746.json"])
