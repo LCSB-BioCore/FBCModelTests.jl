c1148f258786b3f9028cf28f9cf95a81eb659cfc
Mirek Kratochvil <exa.exa@gmail.com>
2022-10-18 13:21:30 +0200

    add a mock test-testing AbstractTestSet

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,6 +8,9 @@ using COBREXA, Tulip
 # this loads some data
 include("data.jl")
 
+# this loads CountTests "mock" tester for testing the tests
+include("testcounter.jl")
+
 @testset "FBCModelTests test suite" begin
     include("frog.jl")
     include("memote.jl")
