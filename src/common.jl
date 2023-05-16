9078c13143a460e566ef7c6793c445014d05c65d
Mirek Kratochvil <exa.exa@gmail.com>
2023-05-16 11:53:53 +0200

    implement a test set where failures don't print out backtraces

--- src/common.jl
+++ src/common.jl
@@ -13,3 +13,40 @@ test_dicts(match::Function, a::Dict, b::Dict) =
             end
         end
     end
+
+struct QuietTestSet <: Test.AbstractTestSet
+    inner::Test.DefaultTestSet
+    QuietTestSet(desc::AbstractString; kwargs...) =
+        new(Test.DefaultTestSet(desc; kwargs...))
+end
+
+Quiet(args...; kwargs...) = QuietTestSet(Test.DefaultTestSet(args...; kwargs...))
+
+function Test.record(ts::QuietTestSet, t::Union{Test.Result,Test.AbstractTestSet})
+    Test.record(ts.inner, t)
+end
+
+function Test.record(
+    ts::QuietTestSet,
+    t::Test.Fail;
+    print_result::Bool = Test.TESTSET_PRINT_ENABLE[],
+)
+    if print_result
+        print(ts.inner.description, ": ")
+        println()
+        if t.test_type !== :test_interrupted
+            println(t)
+        end
+    end
+    push!(ts.inner.results, t)
+    return t
+end
+
+Test.finish(ts::QuietTestSet; kwargs...) = Test.finish(ts.inner; kwargs...)
+Test.print_test_errors(ts::QuietTestSet) = Test.print_test_errors(ts.inner)
+Test.print_test_results(ts::QuietTestSet, depth_pad = 0) =
+    Test.print_test_results(ts.inner, depth_pad)
+Test.get_alignment(ts::QuietTestSet, depth = 0) = Test.get_alignment(ts.inner, depth)
+Test.filter_errors(ts::QuietTestSet) = Test.filter_errors(ts.inner)
+Test.get_test_counts(ts::QuietTestSet) = Test.get_test_counts(ts.inner)
+Test.print_counts(ts::QuietTestSet, args...) = Test.print_counts(ts.inner, args...)
